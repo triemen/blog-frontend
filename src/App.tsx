2f790ac0889d15f1de5626c3ef4c16f6d7c1b275
Tobias Riemenschneider <tobias.riemenschneider@climatepartner.com>
2023-01-03 16:35:05 +0100

    copy frontend from auth0-blog/nest-react-blog-ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Post from './components/post/Post';
+import Edit from './components/post/Edit';
+import Create from './components/post/Create';
 
-function App() {
+
+function App(): JSX.Element {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+      <div className={'container'}>
+        <Switch>
+          <Route path={"/"} exact={true} component={Home} />
+          <Route path={"/post/:postId"} component={Post}/>
+          <Route path={"/edit/:postId"} component={Edit}/>
+          <Route path={"/create"} component={Create} />
+        </Switch>
+      </div>
     </div>
   );
 }
